a18312c8d4b06469cf3416f5b7f92b0d26ba55bd
raguila6 <raguila6@stu.southtexascollege.edu>
2021-11-29 22:20:21 -0600

    chnages

--- src/pages/about.vue
+++ src/pages/about.vue
@@ -8,6 +8,6 @@
         <h1 class="text-xl font-medium">About this...</h1>
     <img src="../assets/warehouse.gif"  alt="HelloBG">
 
-    <div class="content-end text-left text-center bg-gray-200"><p>With this app , is a basic appication to update and store inventory for computer assets</p></div>
+    <div class="content-end text-left  bg-gray-200"><p>With this app , is a basic appication to update and store inventory for computer assets</p></div>
     </div>
 </template>
