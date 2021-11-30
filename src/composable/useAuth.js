23145eebc24beb071700e728b3846dd370cc4a62
raguila6 <raguila6@stu.southtexascollege.edu>
2021-11-29 23:01:37 -0600

    user display working

--- src/composable/useAuth.js
+++ src/composable/useAuth.js
@@ -1,58 +1,54 @@
-import {ref} from "vue";
+import { ref } from "vue";
+import { signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut, GoogleAuthProvider, signInWithPopup,} from "firebase/auth";
 
-import { signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut, GoogleAuthProvider, signInWithPopup } from "@firebase/auth";
-
-import { firebaseApp, firebaseAuth } from "./useFirebase";
-import { async } from "@firebase/util";
-
-
+import { firebaseAuth } from "./useFirebase";
 
 const isAuthenticated = ref(false);
 
 const user = ref("");
 
-
-
 const useAuth = () => {
-
-    const googleLogin = async () => {
-        const provider = new GoogleAuthProvider();
-        const res = await signInWithPopup(firebaseAuth, provider);
-        if (res.user) {
-          isAuthenticated.value = true;
-          user.value = res.user.displayName;
-        }
-       };
-
-    const login = async(username, password) => {
-     const res = 
-     await signInWithEmailAndPassword(firebaseAuth, username, password);
-     
-     if (res.user) {
-       isAuthenticated.value = true;
-       user.value = res.user.email
-     }
-    };
-
-    const signup = async(username, password) => {
-      const res = 
-      await createUserWithEmailAndPassword(firebaseAuth, username, password);
-      
-      if (res.user) {
-        isAuthenticated.value = true;
-        user.value = res.user.email
-      }
-     };
-  
-    const logout = async() => {
-      await signOut(firebaseAuth);
-      isAuthenticated.value = false;
-      user.value = "";
-    };
-
-    
-  
-    return { isAuthenticated, login, signup, logout, user, googleLogin };
+  const googleLogin = async () => {
+    const provider = new GoogleAuthProvider();
+    const credentials = await signInWithPopup(firebaseAuth, provider);
+    if (credentials.user) {
+      isAuthenticated.value = true;
+      user.value = credentials.user.displayName;
+    }
   };
-  
-  export default useAuth;
+  const login = async (username, password) => {
+    const credentials = await signInWithEmailAndPassword(
+      firebaseAuth,
+      username,
+      password
+    );
+
+    if (credentials.user) {
+      isAuthenticated.value = true;
+      user.value = credentials.user.email;
+    }
+  };
+
+  const signup = async (username, password) => {
+    const credentials = await createUserWithEmailAndPassword(
+      firebaseAuth,
+      username,
+      password
+    );
+
+    if (credentials.user) {
+      isAuthenticated.value = true;
+      user.value = credentials.user.email;
+    }
+  };
+
+  const logout = async () => {
+    await signOut(firebaseAuth);
+    isAuthenticated.value = false;
+    user.value = "";
+  };
+
+  return { isAuthenticated, login, signup, logout, user, googleLogin };
+};
+
+export default useAuth;
