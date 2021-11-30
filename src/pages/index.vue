cbfd6859b9928430b08322bc63697eea2edafa6e
raguila6 <raguila6@stu.southtexascollege.edu>
2021-11-29 22:57:32 -0600

    user update

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,12 +1,20 @@
 <script setup>
-//import useAuth from '../composable/useAuth';
-//const { isAuthenticated,user} = useAuth();
+import useAuth from '../composable/useAuth';
+const { isAuthenticated,user} = useAuth();
 </script>
 
 <template>
     
     <div class="flex flex-col items-center object-left-bottom h-screen-nonav">
-        <h2 class="text-xl font-medium">Welcome </h2>
+     <h2
+      v-if="!isAuthenticated"
+      class="text-4xl tracking-tighter font-extralight"
+    >
+      Welcome
+    </h2>
+    <h2 v-else class="text-6xl font-extrabold tracking-tighter">
+      Welcome {{ user }}
+    </h2>
     <img src="../assets/scan.gif"  alt="AboutBG">
     </div>
 </template>
